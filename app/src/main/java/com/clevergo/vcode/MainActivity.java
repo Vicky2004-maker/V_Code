e1ad579edec80e6c12eced33e25e893b7001eae1
Viknesh <73737315+Vicky2004-maker@users.noreply.github.com>
2022-05-11 15:57:05 +0530

    Initial Commit

--- app/src/main/java/com/clevergo/vcode/MainActivity.java
+++ app/src/main/java/com/clevergo/vcode/MainActivity.java
@@ -1,8 +1,12 @@
 package com.clevergo.vcode;
 
+import android.content.Intent;
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new Timer().schedule(new TimerTask() {
+            @Override
+            public void run() {
+                startActivity(new Intent(MainActivity.this, CodeViewActivity.class));
+            }
+        }, 3000);
     }
 }
