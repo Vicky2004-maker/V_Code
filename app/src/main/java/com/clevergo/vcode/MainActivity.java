4121f94fdcd5507249a032d59974813870bd8cbb
Viknesh <73737315+Vicky2004-maker@users.noreply.github.com>
2022-05-11 16:04:16 +0530

    Initial Commit

--- app/src/main/java/com/clevergo/vcode/MainActivity.java
+++ app/src/main/java/com/clevergo/vcode/MainActivity.java
@@ -2,9 +2,12 @@ package com.clevergo.vcode;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.Objects;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -13,7 +16,14 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_main);
+
+        Objects.requireNonNull(getSupportActionBar()).hide();
 
         new Timer().schedule(new TimerTask() {
             @Override
