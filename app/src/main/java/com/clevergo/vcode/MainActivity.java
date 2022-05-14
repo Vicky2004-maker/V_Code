8ca7c0e377bc6e13c737aa9e06afee2da0f7cdad
Viknesh <73737315+Vicky2004-maker@users.noreply.github.com>
2022-05-14 19:53:52 +0530

    Initial Commit

--- app/src/main/java/com/clevergo/vcode/MainActivity.java
+++ app/src/main/java/com/clevergo/vcode/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.Objects;
@@ -25,11 +26,57 @@ public class MainActivity extends AppCompatActivity {
 
         Objects.requireNonNull(getSupportActionBar()).hide();
 
-        new Timer().schedule(new TimerTask() {
+        Helper.initializeFile(MainActivity.this);
+        Helper.updateSettingsMap();
+
+        if(Objects.equals(Helper.settingsMap.get("privacyPolicy"), "agree") && Helper.checkPermissions(MainActivity.this)) {
+            timerToLoad(3000);
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        if (!Helper.settingsMap.containsKey("privacyPolicy")) {
+            AlertDialog.Builder alertDialog = new AlertDialog.Builder(MainActivity.this);
+            alertDialog.setTitle(getString(R.string.privacyPolicy));
+            alertDialog.setMessage(getString(R.string.privacyPolicyMessage));
+            alertDialog.setCancelable(false);
+            alertDialog.setPositiveButton(getString(R.string.agree), (a, b) -> {
+                Helper.writeSetting("privacyPolicy", "agree");
+                if (!Helper.checkPermissions(MainActivity.this)) {
+                    Helper.launchPermission(MainActivity.this);
+                } else {
+                    timerToLoad(1000);
+                }
+            });
+            alertDialog.setNeutralButton(getString(R.string.disagree), (a, b) -> finish());
+            alertDialog.setNegativeButton(getString(R.string.view), (a, b) -> Helper.launchUrlInBrowser(Helper.PRIVACY_POLICY_URL, MainActivity.this));
+
+            alertDialog.create();
+            alertDialog.show();
+        } else if (!Helper.checkPermissions(MainActivity.this)) {
+            Helper.launchPermission(MainActivity.this);
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if (!Helper.checkPermissions(MainActivity.this)) {
+            Helper.launchPermission(MainActivity.this);
+        }
+    }
+
+    private void timerToLoad(int time) {
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
             @Override
             public void run() {
                 startActivity(new Intent(MainActivity.this, CodeViewActivity.class));
             }
-        }, 3000);
+        }, time);
     }
 }
